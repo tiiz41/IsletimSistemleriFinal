62d3e4cb0fb9ca8240ddcf325a9880495d6bf565
Batın Sezer <batn.sezer@gmail.com>
2022-01-19 15:02:49 +0300

    işletim sistemi

---
+++ Scripts-IsletimSistemleriFinalOdevi/createJobFile.ps1
@@ -0,0 +1,39 @@
+﻿<# Bir alt satırdaki kodu tek başınada çalıştırın #>
+
+Set-ExecutionPolicy RemoteSigned
+
+<# Odevi iki farkli algoritma ile yaptigimiz icin her bir algoritma icin ayri ayri schedule job ve powershell Script dosyalari olusturduk. #>
+
+<# 
+
+ Eger size gonderdigimiz "Scripts-IsletimSistemleriFinalOdevi" adli klasor masaustunde degilse bu klasoru masaustune almayi ve asagidaki -Argument kisminde Scriptimizin yolunda bulunan "Batın" yazisini kendi user name'nize gore degistirmenizi
+ rica ederiz.  
+ 
+ #>
+
+<# 1. algoritma için job #>
+
+<# Her gün saat sabah 10:00'da yolunu belirttiğimiz powershell script'ini çalıştıran "algoritmaTask1 adında bir schedule job oluşturuyoruz. #>
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe'  -Argument 'C:\Users\Batın\Desktop\Scripts-IsletimSistemleriFinalOdevi\algoritma1.ps1'
+
+$trigger =  New-ScheduledTaskTrigger -Daily -At 10am
+
+Register-ScheduledTask -Action $action  -Trigger $trigger -TaskName "algoritmaTask1" 
+
+
+
+
+
+
+<# 2. algoritma için job #>
+
+<# Her gün saat sabah 10:00'da yolunu belirttiğimiz powershell script'ini çalıştıran "algoritmaTask2 adında bir schedule job oluşturuyoruz. #>
+
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe'  -Argument 'C:\Users\Batın\Desktop\Scripts-IsletimSistemleriFinalOdevi\algoritma2.ps1'
+
+$trigger =  New-ScheduledTaskTrigger -Daily -At 10am
+
+Register-ScheduledTask -Action $action  -Trigger $trigger -TaskName "algoritmaTask2" 
+
+
